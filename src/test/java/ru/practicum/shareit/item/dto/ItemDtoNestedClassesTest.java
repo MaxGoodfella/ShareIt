0212c3dc594c2d909127f965f945c98554ae45f4
MaximGuseynov <MaxGoodfella@yandex.ru>
2024-05-29 10:50:46 +0500

    Sprint15.1.3 - fix: after 1st review

--- src/test/java/ru/practicum/shareit/item/dto/ItemDtoNestedClassesTest.java
+++ src/test/java/ru/practicum/shareit/item/dto/ItemDtoNestedClassesTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
@@ -24,24 +23,22 @@ class ItemDtoNestedClassesTest {
     @Test
     void testItemBookingDto() throws Exception {
 
-        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now().withNano(0);
 
         ItemDto.ItemBookingDto bookingDto = ItemDto.ItemBookingDto.builder()
                 .id(1)
-                .start(now.minusDays(1))
-                .end(now.plusDays(1))
+                .start(now.minusDays(1).withNano(0))
+                .end(now.plusDays(1).withNano(0))
                 .bookerId(123)
                 .build();
 
         JsonContent<ItemDto.ItemBookingDto> result = jsonBooking.write(bookingDto);
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
-
         assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(1);
         assertThat(result).extractingJsonPathStringValue("$.start")
-                .isEqualTo(now.minusDays(1).format(formatter));
+                .isEqualTo(now.minusDays(1).withNano(0).toString());
         assertThat(result).extractingJsonPathStringValue("$.end")
-                .isEqualTo(now.plusDays(1).format(formatter));
+                .isEqualTo(now.plusDays(1).withNano(0).toString());
         assertThat(result).extractingJsonPathNumberValue("$.bookerId").isEqualTo(123);
 
     }
@@ -49,24 +46,23 @@ class ItemDtoNestedClassesTest {
     @Test
     void testItemCommentDto() throws Exception {
 
-        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now().withNano(0);
 
         ItemDto.ItemCommentDto commentDto = ItemDto.ItemCommentDto.builder()
                 .id(1)
                 .text("text")
                 .authorName("name")
-                .created(now)
+                .created(now.withNano(0))
                 .build();
 
         JsonContent<ItemDto.ItemCommentDto> result = jsonComment.write(commentDto);
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
 
         assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(1);
         assertThat(result).extractingJsonPathStringValue("$.text").isEqualTo("text");
         assertThat(result).extractingJsonPathStringValue("$.authorName").isEqualTo("name");
         assertThat(result).extractingJsonPathStringValue("$.created")
-                .isEqualTo(now.format(formatter));
+                .isEqualTo(now.withNano(0).toString());
 
     }
+
 }
